3f3eca444d74a266288247da6a60da17ac3154da
shenjack <3695888@qq.com>
2023-12-31 03:20:20 +0800

    use 4 space

--- rspress.config.ts
+++ rspress.config.ts
@@ -2,23 +2,25 @@ import * as path from 'path';
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
-  root: path.join(__dirname, 'docs'),
-  title: 'Difficult-Rocket-docs',
-  description: 'Diffuclt Rocket 的文档',
-  icon: "/icon.png",
-  logo: "/icon.png",
-  themeConfig: {
-    socialLinks: [
-      { icon: 'github', mode: 'link', content: 'https://github.com/shenjackyuanjie/DR-docs' },
-      { icon: 'github', mode: 'link', content: 'https://github.com/shenjackyuanjie/Difficult-Rocket' },
-    ],
-  },
-  markdown: {
-      highlightLanguages: [
-          ['py', 'python'],
-          'python',
-          ['cfg', 'ini'],
-          'toml',
-      ]
-  }
+    root: path.join(__dirname, 'docs'),
+    title: 'Difficult-Rocket-docs',
+    description: 'Diffuclt Rocket 的文档',
+    icon: "/icon.png",
+    logo: "/icon.png",
+    themeConfig: {
+        socialLinks: [
+            { icon: 'github', mode: 'link', content: 'https://github.com/shenjackyuanjie/DR-docs' },
+            { icon: 'github', mode: 'link', content: 'https://github.com/shenjackyuanjie/Difficult-Rocket' },
+        ],
+        lastUpdated: true,
+        lastUpdatedText: "最后更新时间",
+    },
+    markdown: {
+        highlightLanguages: [
+            ['py', 'python'],
+            'python',
+            ['cfg', 'ini'],
+            'toml',
+        ]
+    }
 });
